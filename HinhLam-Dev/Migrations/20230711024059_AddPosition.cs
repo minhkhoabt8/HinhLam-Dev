b0a129ef7e18420f3e26e8a77bd39fa2e90b44be
m4c1st <59388870+minhkhoabt8@users.noreply.github.com>
2023-07-11 10:13:03 +0700

    Update Count

---
+++ HinhLam-Dev/Migrations/20230711024059_AddPosition.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HinhLam_Dev.Migrations
+{
+    public partial class AddPosition : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SubMenuCount",
+                table: "SubMenu",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MenuCount",
+                table: "Menu",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ContentCount",
+                table: "Content",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SubMenuCount",
+                table: "SubMenu");
+
+            migrationBuilder.DropColumn(
+                name: "MenuCount",
+                table: "Menu");
+
+            migrationBuilder.DropColumn(
+                name: "ContentCount",
+                table: "Content");
+        }
+    }
+}
