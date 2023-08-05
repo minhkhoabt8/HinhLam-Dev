ccf3a88fc49754ff1d37ac023c5664133589177d
Khoa Le <minhkhoabtr8@gmail.com>
2023-08-05 17:04:23 +0700

    fix bug content service details 11

---
+++ HinhLam-Infrastructure/Repositories/Consult/ConsultRepository.cs
@@ -0,0 +1,20 @@
+﻿using HinhLam_DataObject.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HinhLam_Infrastructure.Repositories.Consult
+{
+    public class ConsultRepository : HinhLamDBContext, IConsultRepository
+    {
+        private readonly HinhLamDBContext _dbContext;
+
+        public ConsultRepository(HinhLamDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        
+    }
+}
---
+++ HinhLam-Infrastructure/Services/Consult/ConsultService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HinhLam_Infrastructure.Services.Consult
+{
+    public class ConsultService : IConsultService
+    {
+
+    }
+}
